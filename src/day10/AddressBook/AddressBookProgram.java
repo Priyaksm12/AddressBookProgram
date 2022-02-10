cd33abdc01e6f3be816fe752e425a9c3b14bb40e
Priyaksm12 <priyankaks.priya@gmail.com>
2022-02-10 17:47:44 +0530

    Welcome To Address Book Program

---
+++ src/day10/AddressBook/AddressBookProgram.java
@@ -0,0 +1,8 @@
+package day10.AddressBook;
+
+public class AddressBookProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book Program");
+    }
+}
